3493b1b2aeefbf4ff06da74b52cf5b9707815baa
Christos Vontas <christos@redsift.io>
2019-09-30 16:52:26 +0100

    node image with capnproto installed

---
+++ root/usr/bin/node-version-dump.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -e
+
+NODE=$(node -v)
+NPM=$(npm -v)
+
+echo "NodeJS ${NODE}"
+echo "NPM ${NPM}"
--- root/usr/bin/tools-version-dump.sh
+++ root/usr/bin/tools-version-dump.sh
@@ -2,14 +2,11 @@
 
 set -e
 
-# NODE=$(node -v)
-# NPM=$(npm -v)
 JAVA=$(java -version)
 MAVEN=$(mvn -version)
 
-# echo "NodeJS ${NODE}"
-# echo "NPM ${NPM}"
 echo "$JAVA"
 echo "$MAVEN"
 
 /usr/bin/go-version-dump.sh
+# /usr/bin/node-version-dump.sh
